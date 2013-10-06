d99109b71b71b4fbb61bdf9db2c205036eb83a04
meganemura <mura2megane@gmail.com>
2013-10-07 04:21:16 +0900

    Part2: Tutorial: Rotate Then Shoot.

--- app/layers/hello_world_layer.rb
+++ app/layers/hello_world_layer.rb
@@ -4,8 +4,10 @@ class HelloWorldLayer < Joybox::Core::LayerColor
 
   def on_enter
     @monsters = []
-    @projectiles = []
     @monster_destroyed = 0
+
+    @projectiles = []
+    @next_projectile = nil
 
     @player = Sprite.new(:file_name => 'arts/player2.png')
     @player.position = [@player.contentSize.width.half, Screen.half_height]
@@ -97,33 +99,31 @@ class HelloWorldLayer < Joybox::Core::LayerColor
 
   def shoot_projectile(touches)
 
+    return if @next_projectile
+
     # Choose one of the touches to work with
     touch = touches.any_object
     location = self.convertTouchToNodeSpace(touch)
 
     # Set up initial location of projectile
-    projectile = Sprite.new(:file_name => 'arts/projectile2.png')
-    projectile.position = [20, Screen.half_height]
-    projectile.tag = 2
-    @projectiles << projectile
+    @next_projectile = Sprite.new(:file_name => 'arts/projectile2.png')
+    @next_projectile.position = [20, Screen.half_height]
 
     # Determine offset of location to projectile
-    offset = location - projectile.position
+    offset = location - @next_projectile.position
 
     # Bail out if you are shooting down or backwards
     return if offset.x <= 0
 
-    # Ok to add now - we've double checked position
-    self << projectile
-
-    real_x = Screen.width + projectile.contentSize.width.half
+    # Determine where you wish to shoot the projectile to
+    real_x = Screen.width + @next_projectile.contentSize.width.half
     ratio = offset.y.to_f / offset.x
-    real_y = real_x * ratio + projectile.position.y
+    real_y = real_x * ratio + @next_projectile.position.y
     real_dest = [real_x, real_y]
 
     # Determine the length of how far you're shooting
-    off_real_x = real_x - projectile.position.x
-    off_real_y = real_y - projectile.position.y
+    off_real_x = real_x - @next_projectile.position.x
+    off_real_y = real_y - @next_projectile.position.y
     length = Math.sqrt(off_real_x**2 + off_real_y**2)
     velocity = 480 / 1  # 480 pixels/second
     real_move_duration = length / velocity
@@ -132,7 +132,19 @@ class HelloWorldLayer < Joybox::Core::LayerColor
     angle_radians = jbpToAngle(jbp(off_real_x, off_real_y))
     angle_degrees = angle_radians / Math::PI * 180.0  # TODO: Add macro to joybox: `motion/joybox/macros.rb`
     angle = -1 * angle_degrees
-    @player.rotation = angle
+    rotate_degrees_per_second = 180 / 0.5   # Would take 0.5 seconds to rotate 180 degrees, or half a circle
+    degrees_diff = @player.rotation - angle
+    rotate_duration = (degrees_diff / rotate_degrees_per_second).abs
+
+    action_rotate = Rotate.to(:angle => angle, :duration => rotate_duration)
+    action_rotate_done = Callback.with do |node|
+      # OK to add now - rotation is finished!
+      self << @next_projectile
+      @projectiles << @next_projectile
+      @next_projectile = nil
+    end
+    sequence_rotate = Sequence.with(:actions => [action_rotate, action_rotate_done])
+    @player.run_action(sequence_rotate)
 
     # Move projectile to actual endpoint
     action_move = Move.to(:position => real_dest, :duration => real_move_duration)
@@ -141,7 +153,9 @@ class HelloWorldLayer < Joybox::Core::LayerColor
       node.removeFromParent
     end
     sequence_action = Sequence.with(:actions => [action_move, action_move_done])
-    projectile.run_action(sequence_action)
+    @next_projectile.run_action(sequence_action)
+
+    @next_projectile.tag = 2
 
     audio_effect = AudioEffect.new
     audio_effect.add(:effect => :pew_pew_lei, :file_name => 'sounds/pew-pew-lei.caf')
